5919c26ead17acc641ef864d36ed48fe1dc5c215
Valhalla Deploy <valhalla@deploy.local>
2026-01-21 14:26:42 -0600

    Revert "Fix Alembic multiple heads: Chain ops_enablers_001 to go_live_core_tables"
    This reverts commit 03b83c18686a3dc7bf9acc07ab814e1fbf867f97.

--- services/api/alembic/versions/20250922_ops_enablers.py
+++ services/api/alembic/versions/20250922_ops_enablers.py
@@ -5,7 +5,7 @@ from sqlalchemy import inspect
 
 # revision identifiers
 revision = "ops_enablers_001"
-down_revision = "20260121_go_live_core_tables"
+down_revision = None
 branch_labels = None
 depends_on = None
 
